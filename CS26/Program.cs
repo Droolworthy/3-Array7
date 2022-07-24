b0b4e8be70650dcede9efd9fd8a09f8c2b9d2d01
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-07-24 14:28:04 +0300

    Добавьте файлы проекта.

---
+++ CS26/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CS26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "You. Win. Some. You. Lose. Some";
+
+            string[] subs = text.Split(' ' , '.');
+
+            foreach (var sub in subs)
+            {
+                Console.WriteLine($"Substring: {sub}");
+            }
+        }
+    }
+}
